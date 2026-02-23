97a9b0ea33dd29d7f51e39548ba91636abc4b80f
abubakir_706 <abubakr20106@gmail.com>
2026-02-23 10:49:31 +0500

    aaaaaaaaaa

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,12 +1,27 @@
+// controllers/categoryController.js fayliga qo'shing:
+
 const Category = require("../models/Category");
+const Product = require("../models/Product"); // Mahsulotlarni ham tekshirish uchun
 
-exports.getCategories = async (req, res) => {
-  const categories = await Category.find();
-  res.json(categories);
-};
+exports.deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-exports.createCategory = async (req, res) => {
-  const category = new Category(req.body);
-  await category.save();
-  res.status(201).json(category);
+    // 1. Kategoriyani topish va o'chirish
+    const category = await Category.findByIdAndDelete(id);
+
+    if (!category) {
+      return res.status(404).json({ message: "Kategoriya topilmadi" });
+    }
+
+    // 2. Ixtiyoriy: Shu kategoriyaga tegishli mahsulotlarni ham o'chirish
+    // (Frontend kodingizda shuni kutayotgan ekan)
+    await Product.deleteMany({ category: id });
+
+    res.json({ message: "Kategoriya va unga tegishli mahsulotlar o'chirildi" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Serverda xatolik yuz berdi", error: error.message });
+  }
 };
