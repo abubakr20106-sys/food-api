58bbf1ea1369bfea87a9b349ee11f8599077b8ee
abubakir_706 <abubakr20106@gmail.com>
2026-02-23 10:51:59 +0500

    afgbht

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,11 +1,15 @@
+// routes/categoryRoutes.js faylini tekshiring va yangilang:
+
 const express = require("express");
 const router = express.Router();
 const {
   getCategories,
   createCategory,
+  deleteCategory,
 } = require("../controllers/categoryController");
 
 router.get("/", getCategories);
 router.post("/", createCategory);
+router.delete("/:id", deleteCategory); // SHU QATORNI QO'SHING
 
 module.exports = router;
