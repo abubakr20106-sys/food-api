cf44f304b7d4c790484bfd0a00687ad5d1727127
abubakir_706 <abubakr20106@gmail.com>
2026-02-27 11:41:17 +0500

    abu

--- controllers/productController.js
+++ controllers/productController.js
@@ -11,8 +11,26 @@ exports.createProduct = async (req, res) => {
   res.status(201).json(product);
 };
 
+// Mahsulotni tahrirlash
+exports.updateProduct = async (req, res) => {
+  try {
+    const updated = await Product.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true, runValidators: true },
+    ).populate("category");
+
+    if (!updated) {
+      return res.status(404).json({ message: "Mahsulot topilmadi" });
+    }
+
+    res.json(updated);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
 exports.deleteProduct = async (req, res) => {
   await Product.findByIdAndDelete(req.params.id);
   res.json({ message: "Mahsulot o'chirildi" });
 };
-    
